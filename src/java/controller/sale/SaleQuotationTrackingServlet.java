3c65127638d5351644c947b09e620ec6c64a1dba
locnq <loc256204@gmail.com>
2026-02-16 09:52:29 +0700

    update

--- src/java/controller/sale/SaleQuotationTrackingServlet.java
+++ src/java/controller/sale/SaleQuotationTrackingServlet.java
@@ -17,13 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(name="SaleQuotationTrackingServlet", urlPatterns={"/sale/quotation/tracking"})
 public class SaleQuotationTrackingServlet extends HttpServlet {
    
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -41,14 +35,7 @@ public class SaleQuotationTrackingServlet extends HttpServlet {
         }
     } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
